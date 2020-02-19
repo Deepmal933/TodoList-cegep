c2c43d34ec5698818fd6c784cb397ef59f848cb2
VishalBhardwaj1408 <vishaalbhardwaji8654@gmail.com>
2020-02-19 14:48:22 -0500

    added login page

---
+++ app/src/main/java/com/example/todolist/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView user_name;
+    private TextView user_pass;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        user_name = findViewById(R.id.user_email);
+        user_pass = findViewById(R.id.user_pass);
+        login=findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(user_name.getText().equals("") || user_pass.getText().equals("")){
+                    Toast.makeText(LoginActivity.this,"Please Enter User name Password",Toast.LENGTH_LONG).show();
+                }
+                else if(user_name.getText().toString().equals(Profile.EMAIL) && user_pass.getText().toString().equals(Profile.PASS)){
+                    Intent in = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(in);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Invalid login",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
