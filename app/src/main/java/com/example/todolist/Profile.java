fff6292cb245ac8c6e18e7416827cb8e7985bd49
saiakshay <Divya@2808>
2020-02-19 14:25:58 -0500

    fix profile activity

---
+++ app/src/main/java/com/example/todolist/Profile.java
@@ -0,0 +1,8 @@
+package com.example.todolist;
+
+public class Profile {
+
+    public static final String NAME = "John Doe";
+    public static final String PASS = "demo";
+    public static final String EMAIL = "demo@demo.com";
+}
